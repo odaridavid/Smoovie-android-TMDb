f0c245d7628616e4e44d378704dde4bd1f6eeda4
David Odari <davidkibzodari@gmail.com>
2018-07-30 19:11:25 +0300

    UI Update

--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/MainActivity.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/MainActivity.java
@@ -1,20 +1,112 @@
 package smoovie.apps.com.kayatech.smoovie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.List;
+
 import smoovie.apps.com.kayatech.smoovie.Model.Movie;
+import smoovie.apps.com.kayatech.smoovie.Network.OnMoviesCallback;
+import smoovie.apps.com.kayatech.smoovie.Network.TMDBMovies;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    MoviesAdapter mMoviesAdapter;
+    RecyclerView mMoviesRecyclerView;
+
+    private TMDBMovies movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        movieList = TMDBMovies.getInstance();
+
+        //Reference
+        mMoviesRecyclerView = findViewById(R.id.movie_recycler_view);
+        mMoviesRecyclerView.setHasFixedSize(true);
+
+
+        //Grid Layout For Main View
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        mMoviesRecyclerView.setLayoutManager(gridLayoutManager);
+
+        movieList.getMovies(new OnMoviesCallback() {
+            @Override
+            public void onSuccess(List<Movie> movies) {
+                // Loads Movie Data
+                mMoviesAdapter = new MoviesAdapter(getApplicationContext(), movies);
+                mMoviesRecyclerView.setAdapter(mMoviesAdapter);
+            }
+
+            @Override
+            public void onFailure() {
+                Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Fail to Load");
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_sort_high_rating) {
+
+//            //methods
+//            ApiGetServices service = retrofit.create(ApiGetServices.class);
+//            Call<MovieListResponse> movieCall = service.getHighRatedMovies();
+//            movieCall.enqueue(new Callback<MovieListResponse>() {
+//                @Override
+//                public void onResponse(Call<MovieListResponse> call, Response<MovieListResponse> response) {
+//                    mMoviesAdapter.setMovieList(response.body().getMoviesResult());
+//                }
+//
+//                @Override
+//                public void onFailure(Call<MovieListResponse> call, Throwable t) {
+//                   Log.d(TAG,"Fail to retrieve high rated");
+//                }
+//            });
+//
+//           // Toast.makeText(MainActivity.this,"Highest Rating Movies",Toast.LENGTH_LONG).show();
+
+
+        } else if (itemId == R.id.action_sort_most_popular) {
+
+//            //methods
+//            ApiGetServices service = retrofit.create(ApiGetServices.class);
+//            Call<MovieListResponse> movieCall = service.getPopularMovies();
+//            movieCall.enqueue(new Callback<MovieListResponse>() {
+//                @Override
+//                public void onResponse(Call<MovieListResponse> call, Response<MovieListResponse> response) {
+//                    mMoviesAdapter.setMovieList(response.body().getMoviesResult());
+//                }
+//
+//                @Override
+//                public void onFailure(Call<MovieListResponse> call, Throwable t) {
+//                    Log.d(TAG,"Fail to retrieve popular");
+//                }
+//            });
+//
+//          //  Toast.makeText(MainActivity.this,"Most Popualar Movies",Toast.LENGTH_LONG).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/Model/Movie.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/Model/Movie.java
@@ -1,9 +1,85 @@
 package smoovie.apps.com.kayatech.smoovie.Model;
 
 
-public class Movie  {
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    //model class to map json data to pojo
+    @SerializedName("original_title")
+    private String MovieTitle;
+
+    @SerializedName("overview")
+    private String MovieOverview;
+
+    @SerializedName("release_date")
+    private String MovieReleaseDate;
+
+    @SerializedName("poster_path")
+    private String MoviePoster;
+
+    @SerializedName("vote_average")
+    private float VoterAverage;
+
+    @SerializedName("backdrop_path")
+    private String Backdrop;
+
+    Movie() {
+    }
+
+    Movie(String movieTitle, String movieOverview, String movieReleaseDate, String moviePoster, float voterAverage) {
+        MovieTitle = movieTitle;
+        MovieOverview = movieOverview;
+        MovieReleaseDate = movieReleaseDate;
+        MoviePoster = moviePoster;
+        VoterAverage = voterAverage;
+    }
 
 
+    public String getMovieTitle() {
+        return MovieTitle;
+    }
 
+    public void setMovieTitle(String movieTitle) {
+        MovieTitle = movieTitle;
+    }
 
+    public String getMovieOverview() {
+        return MovieOverview;
+    }
+
+    public void setMovieOverview(String movieOverview) {
+        MovieOverview = movieOverview;
+    }
+
+    public String getMovieReleaseDate() {
+        return MovieReleaseDate;
+    }
+
+    public void setMovieReleaseDate(String movieReleaseDate) {
+        MovieReleaseDate = movieReleaseDate;
+    }
+
+    public String getMoviePoster() {
+        return MoviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        MoviePoster = moviePoster;
+    }
+
+    public float getVoterAverage() {
+        return VoterAverage;
+    }
+
+    public void setVoterAverage(float voterAverage) {
+        VoterAverage = voterAverage;
+    }
+
+    public String getBackdrop() {
+        return Backdrop;
+    }
+
+    public void setBackdrop(String backdrop) {
+        Backdrop = backdrop;
+    }
 }
