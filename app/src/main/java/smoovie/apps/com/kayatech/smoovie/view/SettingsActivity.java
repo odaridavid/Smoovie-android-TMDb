9ae89184576583f9e411e103527c1e2f7d2112d2
David Odari <davidkibzodari@gmail.com>
2018-09-02 23:54:12 +0300

    [FIX] Settings Screen Recreate on Language Change

--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/view/SettingsActivity.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/view/SettingsActivity.java
@@ -1,16 +1,22 @@
 package smoovie.apps.com.kayatech.smoovie.view;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import smoovie.apps.com.kayatech.smoovie.R;
+import smoovie.apps.com.kayatech.smoovie.network.TMDBMovies;
 
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     @Override
@@ -25,6 +31,7 @@ public class SettingsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -38,4 +45,16 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_language_key))) {
+          recreate();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
