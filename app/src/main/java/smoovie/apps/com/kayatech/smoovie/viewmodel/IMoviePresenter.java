033a2e81be6e92dede1346ef9d39424fc48260d1
David Odari <davidkibzodari@gmail.com>
2018-09-04 20:46:40 +0300

    [FEATURE] Added Movie Video Response and Review Response Callbacks
    - Refactored method signatures for methods in interface

--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/IMoviePresenter.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/IMoviePresenter.java
@@ -3,17 +3,26 @@ package smoovie.apps.com.kayatech.smoovie.viewmodel;
 import android.arch.lifecycle.LiveData;
 
 import smoovie.apps.com.kayatech.smoovie.model.Movie;
-import smoovie.apps.com.kayatech.smoovie.model.MovieReviews;
-import smoovie.apps.com.kayatech.smoovie.model.MovieVideos;
 
 
 public interface IMoviePresenter {
 
-    //Method Overload for getMovies Details
+    //Method for getMovies Details
     LiveData<Movie> getMovie(int movieId, final IMovieDetailsCallback iMovieDetailsCallback);
 
-    LiveData<MovieVideos> getMovieVideos(int movieId, final IMovieTrailersCallback iMovieTrailersCallback);
+    /**
+     *
+     * @param movieId Clicked Movie Id
+     * @param iMovieVideosCallback Trailer Callback for handling on success and error
+     */
+    void getMovieVideos(int movieId, final IMovieVideosCallback iMovieVideosCallback);
 
+    /**
+     *
+     * @param page Current Page
+     * @param movieId Clicked Movie Id
+     * @param iMovieReviewsCallback Callback for handling on success and on error for Reviews
+     */
     void getMovieReviews(int page,int movieId, final IMovieReviewsCallback iMovieReviewsCallback);
 
 
--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/MoviesRepository.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/MoviesRepository.java
@@ -9,6 +9,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import smoovie.apps.com.kayatech.smoovie.model.Movie;
+import smoovie.apps.com.kayatech.smoovie.model.MovieReviewResponse;
+import smoovie.apps.com.kayatech.smoovie.model.MovieVideoResponse;
 import smoovie.apps.com.kayatech.smoovie.network.TMDBMovies;
 
 
@@ -17,23 +19,23 @@ public class MoviesRepository implements IMoviePresenter {
 
     //Details
     @Override
-    public LiveData<Movie> getMovie(int movieId, final MovieDetailsCallback movieDetailsCallback) {
+    public LiveData<Movie> getMovie(int movieId, final IMovieDetailsCallback IMovieDetailsCallback) {
 
         final MutableLiveData<Movie> movieDetailLiveData = new MutableLiveData<>();
-        //TODO To Be Checked
+
         TMDBMovies.tmdbApiService
-                .getMovie(movieId,TMDBMovies.API_KEY,TMDBMovies.LANGUAGE)
+                .getMovie(movieId, TMDBMovies.API_KEY, TMDBMovies.LANGUAGE)
                 .enqueue(new Callback<Movie>() {
                     @Override
                     public void onResponse(@NonNull Call<Movie> call, @NonNull Response<Movie> response) {
                         if (response.isSuccessful()) {
-                            Movie mv = response.body();
-                            if (mv != null) {
-                                movieDetailsCallback.onSuccess(mv);
-                                movieDetailLiveData.setValue(mv);
-                                Log.d("RESPONSE", movieDetailLiveData.toString());
+                            Movie movieDetailResponse = response.body();
+                            if (movieDetailResponse != null) {
+                                IMovieDetailsCallback.onSuccess(movieDetailResponse);
+                                movieDetailLiveData.setValue(movieDetailResponse);
+                                Log.d("MOVIE RESPONSE", movieDetailLiveData.toString());
                             } else {
-                                movieDetailsCallback.onError();
+                                IMovieDetailsCallback.onError();
                                 Log.d("MOVIE ERRROR RESPONSE", "NULL");
                             }
                         }
@@ -45,6 +47,72 @@ public class MoviesRepository implements IMoviePresenter {
                     }
                 });
         return movieDetailLiveData;
+    }
+
+
+    //Videos
+    @Override
+    public void getMovieVideos(int movieId, final IMovieVideosCallback iMovieVideosCallback) {
+        Callback<MovieVideoResponse> call = new Callback<MovieVideoResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieVideoResponse> call,@NonNull Response<MovieVideoResponse> response) {
+                if (response.isSuccessful()) {
+                    MovieVideoResponse movieVideoResponse = response.body();
+                    if (movieVideoResponse != null && movieVideoResponse.getMoviesResult() != null) {
+                        //Gets Movies List
+                        iMovieVideosCallback.onSuccess(movieVideoResponse.getMoviesResult());
+
+                    } else {
+                        iMovieVideosCallback.onError();
+                    }
+                } else {
+                    iMovieVideosCallback.onError();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieVideoResponse> call,@NonNull Throwable t) {
+
+            }
+        };
+
+
+        TMDBMovies.tmdbApiService.getMovieVideos(movieId,TMDBMovies.API_KEY,TMDBMovies.LANGUAGE)
+                .enqueue(call);
+    }
+
+
+
+    //Reviews
+    @Override
+    public void getMovieReviews(int page,int movieId, final IMovieReviewsCallback iMovieReviewsCallback) {
+
+        Callback<MovieReviewResponse> call = new Callback<MovieReviewResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieReviewResponse> call,@NonNull Response<MovieReviewResponse> response) {
+                if (response.isSuccessful()) {
+                    MovieReviewResponse moviesReviewResponse = response.body();
+                    if (moviesReviewResponse != null && moviesReviewResponse.getMoviesResult() != null) {
+                        //Gets Movies List
+                        iMovieReviewsCallback.onSuccess(moviesReviewResponse.getPage(), moviesReviewResponse.getMoviesResult());
+
+                    } else {
+                       iMovieReviewsCallback.onFailure();
+                    }
+                } else {
+                    iMovieReviewsCallback.onFailure();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieReviewResponse> call,@NonNull Throwable t) {
+
+            }
+        };
+
+       TMDBMovies.tmdbApiService.getMovieReviews(movieId,TMDBMovies.API_KEY,TMDBMovies.LANGUAGE)
+                .enqueue(call);
+
     }
 
 
