6953607a8da96ebae005af2049c48c062d8d75c2
David Odari <davidkibzodari@gmail.com>
2018-09-04 21:19:08 +0300

    [REFACTOR] Code Spacing

--- app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/MovieDetailViewModel.java
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/viewmodel/MovieDetailViewModel.java
@@ -11,22 +11,16 @@ import smoovie.apps.com.kayatech.smoovie.model.Movie;
 public class MovieDetailViewModel extends AndroidViewModel {
 
     private LiveData<Movie> movie;
-    private final MoviesRepository moviesRepository;
-
+    public final MoviesRepository moviesRepository;
 
     public MovieDetailViewModel(@NonNull Application application) {
         super(application);
-         this.moviesRepository = new MoviesRepository();
+        this.moviesRepository = new MoviesRepository();
 
     }
 
-
-    public void init(int mMovieId, final MovieDetailsCallback movieDetailsCallback) {
-
-
-        movie = moviesRepository.getMovie(mMovieId,movieDetailsCallback);
-
-
+    public void init(int mMovieId, final IMovieDetailsCallback IMovieDetailsCallback) {
+        movie = moviesRepository.getMovie(mMovieId, IMovieDetailsCallback);
     }
 
     public LiveData<Movie> getMovie() {
