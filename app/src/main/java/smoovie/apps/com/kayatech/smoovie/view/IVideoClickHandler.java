2a5985d50f7a247c95519d6772c62ecca9efa71b
David Odari <davidkibzodari@gmail.com>
2018-09-04 20:47:28 +0300

    [FEATURE] Added Video Adapter and on click interface handler

---
+++ app/src/main/java/smoovie/apps/com/kayatech/smoovie/view/IVideoClickHandler.java
@@ -0,0 +1,8 @@
+package smoovie.apps.com.kayatech.smoovie.view;
+
+
+import smoovie.apps.com.kayatech.smoovie.model.MovieVideos;
+
+public interface IVideoClickHandler {
+    void onClick(MovieVideos movieVideos);
+}
